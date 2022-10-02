cfa76cc8d93c158f0b9a9cd6929243bec48c4128
LIOU <aa78789898tw@gmail.com>
2022-10-03 00:56:58 +0800

    add print

--- structTest/demo07.go
+++ structTest/demo07.go
@@ -1,5 +1,7 @@
 package structTest
 
+import "fmt"
+
 func Run7() {
 	var p1 *Person = &Person{
 		Name: "p11",
@@ -12,4 +14,7 @@ func Run7() {
 	p1.Introduce() //Hi, I'm p11
 	p2.Introduce() //Hi, I'm p22
 	p1.Introduce() //Hi, I'm p11
+
+	fmt.Println(&p1.Name) //0xc000104390
+	fmt.Println(&p2.Name) //0xc0001043a0
 }
--- structTest/demo08.go
+++ structTest/demo08.go
@@ -1,5 +1,7 @@
 package structTest
 
+import "fmt"
+
 func Run8() {
 	t1 := Person{
 		Name: "t11",
@@ -12,4 +14,7 @@ func Run8() {
 	t1.Introduce() //Hi, I'm t11
 	t2.Introduce() //Hi, I'm t22
 	t1.Introduce() //Hi, I'm t11
+
+	fmt.Println(&t1.Name) //0xc0001043e0
+	fmt.Println(&t2.Name) //0xc0001043f0
 }
